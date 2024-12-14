08020dbbe18951966513022a4c9ac1e30c2d7500
DZ2K-T <ttoan2710@gmail.com>
2024-12-14 13:04:25 +0700

    test

--- OneDrive/Desktop/cybersoft-FE-ReactJs-capstone/cybersoft-FE-react-capstone/vite-project/src/_page/AdminTeplate/DeleteFilm/duck/reducer.js
+++ OneDrive/Desktop/cybersoft-FE-ReactJs-capstone/cybersoft-FE-react-capstone/vite-project/src/_page/AdminTeplate/DeleteFilm/duck/reducer.js
@@ -1,47 +1,43 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../../../services/apiService";
 
-
-
 export const deleteFilm = createAsyncThunk(
-    "listmovie/deletefilm",
-    async (id_film, { rejectWithValue }) => {
-        try {
-            await api.delete(`QuanLyPhim/XoaPhim?MaPhim=${id_film}`)
-            return id_film;
-        } catch (error) {
-            return rejectWithValue(error.response?.data || error.message);
-        }
+  "listmovie/deletefilm",
+  async (id_film, { rejectWithValue }) => {
+    try {
+      await api.delete(`QuanLyPhim/XoaPhim?MaPhim=${id_film}`);
+      return id_film;
+    } catch (error) {
+      return rejectWithValue(error);
     }
-
+  }
 );
 
-
 const initialState = {
-    loading: false,
-    data: [],
-    error: null,
+  loading: false,
+  data: null,
+  error: null,
 };
 
-
 const deleteFilmReducer = createSlice({
-    name: "deleteFilmReducer",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(deleteFilm.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(deleteFilm.fulfilled, (state, action) => {
-            state.loading = false;
-            state.data = action.payload;
+  name: "deleteFilmReducer",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteFilm.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(deleteFilm.fulfilled, (state, action) => {
+        state.loading = false;
 
-        });
-        builder.addCase(deleteFilm.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        });
-    },
+        state.data = state.data.filter((film) => film.id !== action.payload);
+      })
+      .addCase(deleteFilm.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export default deleteFilmReducer.reducer;
--- OneDrive/Desktop/cybersoft-FE-ReactJs-capstone/cybersoft-FE-react-capstone/vite-project/src/_page/AdminTeplate/DeleteFilm/index.jsx
+++ OneDrive/Desktop/cybersoft-FE-ReactJs-capstone/cybersoft-FE-react-capstone/vite-project/src/_page/AdminTeplate/DeleteFilm/index.jsx
@@ -2,15 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
 import { deleteFilm } from "../DeleteFilm/duck/reducer";
+import { useState } from "react";
 
 const DeleteFilm = ({ filmId }) => {
   const dispatch = useDispatch();
+  const [films, setFilms] = useState();
 
   const handleDelete = () => {
     const confirmDelete = window.confirm("Bạn có chắc chắn muốn xóa phim này?");
     if (confirmDelete) {
-      dispatch(deleteFilm(filmId)); // Gọi hành động xóa
+      setFilms(films.filter((film) => film.id !== filmId));
+      dispatch(deleteFilm(filmId));
     }
+    console.log(filmId);
   };
 
   return (
